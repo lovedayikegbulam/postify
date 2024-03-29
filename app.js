53834280affa9c8c763b5fa2ffac919a615113b2
Loveday Ikegbulam <lovedayikegbulam515@gmail.com>
2024-03-29 08:44:58 +0100

    Initial commit

---
+++ app.js
@@ -0,0 +1,18 @@
+import express from 'express';
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Routes
+app.get('/', (req, res) => {
+  res.send('Welcome to the Express server!');
+});
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
