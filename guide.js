94420b83aeff582b2d436c99338a8f4239937198
Loveday Ikegbulam <lovedayikegbulam515@gmail.com>
2024-04-22 08:37:44 +0100

    rename file

--- guide.js
+++ guide.js
@@ -3,36 +3,36 @@ const data = {
   "name": "Loveday Ikegbulam",
   "email": "lovedayikegbulam515@gmail.com",
   "projectRepo": "https://github.com/lovedayikegbulam/postify",
-  "baseUrl": "https://postify-qra1.onrender.com",
   "Routes": {
     "authRoute": {
       "method": "POST",
-      "register": `${baseUrl}/api/auth/register`,
-      "login": `${baseUrl}/api/auth/login`
+      "register": "https://postify-qra1.onrender.com/api/auth/register",
+      "login": "https://postify-qra1.onrender.com/api/auth/login"
     },
     "postRoute": {
       "createPost": {
         "method": "POST",
-        "url": `${baseUrl}/api/posts/create`
+        "url": "https://postify-qra1.onrender.com/api/posts/create"
       },
       "updatePost": {
         "method": "PATCH",
-        "url": `${baseUrl}/api/posts/postId`
+        "url": "https://postify-qra1.onrender.com/api/posts/postId"
       },
       "getAllPosts": {
         "method": "GET",
-        "url": `${baseUrl}/api/posts`
+        "url": "https://postify-qra1.onrender.com/api/posts"
       },
       "getPostById": {
         "method": "GET",
-        "url": `${baseUrl}/api/posts/postId`
+        "url": "https://postify-qra1.onrender.com/api/posts/postId"
       },
       "deletePost": {
         "method": "DELETE",
-        "url": `${baseUrl}/api/posts/postId`
+        "url": "https://postify-qra1.onrender.com/api/posts/postId"
       }
     }
   }
 }
+
 
 export default data;
