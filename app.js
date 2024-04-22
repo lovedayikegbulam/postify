f5f075f27f23ab7d8b6c69690c3beb3a74fdeca6
Loveday Ikegbulam <lovedayikegbulam515@gmail.com>
2024-04-22 08:27:17 +0100

    change file name

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ import CONFIG from "./config/config.js";
 import connectToDb from "./database/connection.js";
 import userRoute from "./routes/users.route.js";
 import postRoute from "./routes/posts.route.js";
-import data from "./guide.js";
+import data from "./res.data.js";
 
 const app = express();
 const PORT = CONFIG.PORT || 3000;
@@ -22,7 +22,7 @@ app.use("/api/posts", postRoute);
 // Handle the base route
 app.get("", (req, res) => {
   res.status(404);
-  res.json({});
+  res.json(data);
 });
 
 // catch all route
